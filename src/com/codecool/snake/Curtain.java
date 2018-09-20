d8d3c3824dc4cf159bedaddde64ba8563e84428f
Ewelina Kijanowska <kijanowska@gmail.com>
2018-09-20 10:27:17 +0200

    game over screen

--- src/com/codecool/snake/Curtain.java
+++ src/com/codecool/snake/Curtain.java
@@ -1,4 +1,23 @@
 package com.codecool.snake;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
 public class Curtain {
+
+    private static ImageView curtain = new ImageView();
+
+    public static void set(Pane game, Image image) {
+        curtain.setImage(image);
+        curtain.setFitWidth(Globals.WINDOW_WIDTH);
+        curtain.setFitHeight(Globals.WINDOW_HEIGHT);
+//        curtain.setPreserveRatio(true);
+        game.getChildren().add(curtain);
+    }
+
+    public static void remove(Game game) {
+        //remove last element
+        game.getChildren().remove(game.getChildren().size()-1);
+    }
 }
